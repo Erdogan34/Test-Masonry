b0143752129abf9326a346f4504475667f8f8f56
erdogan34 <erdoganahmet@outlook.com>
2020-05-19 14:13:40 +0300

    model is added

---
+++ TestApp/Model/Announcement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TestApp.Model
+{
+    public partial class Announcement
+    {
+        public int? ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+    }
+}
